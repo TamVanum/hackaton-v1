b488a3b6cc5883c858428588fbcdbc1eddafb153
TamVanum <gaston.cath14@gmail.com>
2025-07-27 20:40:05 -0400

    fix: cors all origin allowed

--- backend/cmd/config/server.go
+++ backend/cmd/config/server.go
@@ -18,11 +18,11 @@ func SetupRouter(registrationHandler *rest.RegistrationHandler) *chi.Mux {
 	router.Use(middleware.Logger)
 	router.Use(middleware.Recoverer)
 	router.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"http://localhost:5173", "http://localhost:3000"},
+		AllowedOrigins:   []string{"*"},
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
+		AllowCredentials: false,
 		MaxAge:           300,
 	}))
 
