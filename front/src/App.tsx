177ff676a4b1e06fd33ca4890fa95c95ff552bba
TamVanum <gaston.cath14@gmail.com>
2025-07-31 22:47:02 -0400

    fix: imports app.tsx

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,15 +1,12 @@
 import './App.css'
 import Hero from './components/landing/Hero'
 import About from './components/landing/About'
-import Timeline from './components/landing/Timeline'
-
 
 function App() {
   return (
     <div className="bg-gray-900">
       <Hero />
       <About />
-      <Timeline />
     </div>
   )
 }
